077b0cbfe30e9dd7367220fd71f876cd6d1022f4
Alesh Houdek <mail@alesh.com>
2018-02-07 06:25:48 -0700

    completed initial database migration

---
+++ app/Cat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Catbook;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cat extends Model {
+
+  protected $fillable = ['name', 'date_of_birth', 'breed_id'];
+
+  public function breed() {
+    return $this->belongsTo('Catbook\Breed');
+  }
+
+}
