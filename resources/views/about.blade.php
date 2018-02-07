a2faf319864676ca1656635e240a3d95752fff1f
Alesh Houdek <mail@alesh.com>
2018-02-07 11:10:47 -0700

    added first child view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,7 @@
-<h2>About this site</h2>
-<p>There are over
-  <?php echo $number_of_cats; ?>
-  cats on this site!</p>
+@extends('layouts.master')
+@section('header')
+  <h2>About this site</h2>
+@stop
+@section('content')
+  <p>There are over {{ $number_of_cats }} cats on this site!</p>
+@stop
